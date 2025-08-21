634ad38432128d3ce7692df036c3d0bdf42354ac
shafikunduzi <kunduzishafim2i@gmail.com>
2025-08-21 09:10:59 +0200

    ajouter mon portfolio

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+
+const sections = document.querySelectorAll('section');
+const linksNav = document.querySelectorAll('.navigation a');
+const header = document.querySelector("header");
+const btnHome = document.querySelector('.btn-home');
+const menulcon = document.querySelector("#menu-burger");
+const nav = document.querySelector('.navigation');
+
+const burgerActive = () => {
+    menulcon.classList.toggle('bx-x');
+    nav.classList.toggle('active');
+}
+
+const scrollActive = () => {
+    sections.forEach(section => {
+        let top = window.scrollY;
+        let offset = section.offsetTop - 150;
+        let height = section.offsetHeight;
+        let id = section.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            linksNav.forEach(link => {
+                link.classList.remove('active')
+                document.querySelector(`.navigation a[href*=${id}]`)?.classList.add('active')
+            })
+            
+        }
+    })
+}
+
+
+ScrollReveal({
+    reset:true,
+    distance:'80px',
+    duration: 2000,
+    delay:200
+});
+
+ScrollReveal().reveal('.home-content,.section-title', {origin:"top"})
+ScrollReveal().reveal('.home-img, .services-content, .portfolio-box, .contact-form', {origin:"bottom"})
+// ScrollReveal().reveal('.home-content h1, .about-img', {origin:"left"})
+// ScrollReveal().reveal('.home-content p, .about-content', {origin:"right"})
+
+const typed = new Typed('.multiple', {
+    strings: ["Développeur web", "Web Designer","Développeur d'application "],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+menulcon.addEventListener('click', burgerActive);
+window.addEventListener('scroll', scrollActive);
+if (window.scrollY > 100) {
+    header.classList.add('sticky');
+} else {
+    header.classList.remove('sticky');
+}
